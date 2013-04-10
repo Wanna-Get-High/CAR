bbdf47c654098e30c9c3203248cec71f2b4829a7
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2013-04-10 19:02:27 +0200

    code done

--- tp/RMI/src/SiteImpl.java
+++ tp/RMI/src/SiteImpl.java
@@ -8,16 +8,34 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * I am the implementation of the remote interface.
+ * I am both the RMI server and client.
+ * 
+ * @author Benjamin Van Ryseghem, François Lepan
+ *
+ */
 public class SiteImpl extends UnicastRemoteObject implements SiteItf {
-
-	/**
-	 * 
-	 */
+	
+	/** The command string to add a father to this */
+	protected final String FATHER_STRING = "addFather";
+	
+	/** The command string to display the number of fathers of this*/
+	protected final String FATHERS_SIZE_STRING = "fathersSize";
+	
+	/** The command string to display the number of children of this*/
+	protected final String CHILDREN_SIZE_STRING = "childrenSize";
+	
+	/** auto generated serial */
 	private static final long serialVersionUID = 9118194714523855257L;
 	
-	private SiteItf father;
+	/** The list of fathers */
+	private List<SiteItf> fathers;
+	
+	/** The id used for binding */
 	private String id;
+	
+	
 	private List<SiteItf> children;
 
 	
@@ -26,6 +44,7 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 		
 		this.id = id;
 		this.children = new ArrayList<SiteItf>();
+		this.fathers = new ArrayList<SiteItf>();
 		
 		this.register(id, father);
 		this.loop();
@@ -35,19 +54,54 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 		while(true){
 			String message;
 			Console console = System.console();
-	        message = console.readLine("Please enter user name : ");
-	        if("exit".equals(message)){
-	        	exit();
-	        }
-	        this.sendMessage(message);
+	        message = console.readLine("Please enter a message or a command\n" +
+	        		"\t[addFather host | childrenSize | fathersSize | exit] :\n");
+	        if("exit".equals(message))  						exit();
+	        else if("-h".equals(message))						System.out.println(SiteImpl.stringUsage());
+	        else if("--help".equals(message))					System.out.println(SiteImpl.stringUsage());
+	        else if(message.startsWith(FATHER_STRING))			addFather(message);
+	        else if(message.startsWith(FATHERS_SIZE_STRING))	printFathersSize();
+	        else if(message.startsWith(CHILDREN_SIZE_STRING))	printChildrenSize();
+	        else 												this.sendMessage(message);
 		}
 	}
 
-	private void exit() throws RemoteException {
-		for(SiteItf son : this.children){
-			son.setFather(this.father);
+	
+	protected void printFathersSize(){
+		System.out.println("Number of fathers: "+this.fathers.size());
+	}
+	
+	protected void printChildrenSize(){
+		System.out.println("Number of children: "+this.children.size());
+	}
+	
+	protected void addFather(String message) throws RemoteException{
+		
+		String father = message.substring(FATHER_STRING.length()+1);
+		
+		try{
+			this.addFather((SiteItf)( Naming.lookup(father) ));
 		}
-		this.father.removeSon(this);
+		catch(NotBoundException ex){
+			System.out.println("This father is not in the registry table");
+		}
+		catch(MalformedURLException ex){
+			System.out.println("The URL provided as father is not a valid URL");
+		}
+		System.out.println("Father "+father+ " added");
+	}
+	
+	protected void exit() throws RemoteException {
+			
+		for(SiteItf son : this.children){
+			son.addAllFathers(this.fathers);
+			son.removeFather(this);
+		}
+		
+		for(SiteItf father : this.fathers){
+			father.removeSon(this);		
+		}
+		
 		try {
 			Naming.unbind(this.id);
 		} 
@@ -59,6 +113,7 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 			System.out.println("The URL provided as id is not a valid URL");
 			System.exit(1);
 		}
+		System.out.println("Bye bye");
 		System.exit(0);
 	}
 	
@@ -71,12 +126,11 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 			System.out.println("The URL provided as id is not a valid URL");
 			System.exit(1);
 		}
-		if (father == null){
-			this.father = null;
-			return;
-		}
+		
+		if (father == null) return;
+
 		try{
-			this.setFather((SiteItf)( Naming.lookup(father) ));
+			this.addFather((SiteItf)( Naming.lookup(father) ));
 		}
 		catch(NotBoundException ex){
 			System.out.println("This father is not in the registry table");
@@ -88,17 +142,36 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 		}
 	}
 	
-	@Override
+	
 	public void sendMessage(String message) throws RemoteException {
+		
 		System.out.println("id: "+this.id+" mess: "+message);
 
 		for (SiteItf son : this.children) {
-			son.sendMessage(message);
+			ArrayList<SiteItf> from = new ArrayList<SiteItf>();
+			from.add(this);
+			son.sendMessage(from, message);
+		}
+	}
+	
+	@Override
+	public void sendMessage(List<SiteItf> from, String message) throws RemoteException {
+			
+		System.out.println("id: "+this.id+" mess: "+message);
+		
+		from.add(this);
+		for (SiteItf son : this.children) {
+			if(! from.contains(son)){		
+				son.sendMessage(from, message);
+			}
 		}
 	}
 
 	@Override
 	public void addSon(SiteItf son) throws RemoteException {
+		if(this.children.contains(son)) return;
+		if(this.equals(son)) return;
+		
 		this.children.add(son);
 	}
 	
@@ -135,8 +208,12 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 	}
 
 	@Override
-	public void setFather(SiteItf father) throws RemoteException {
-		this.father = father;
+	public void addFather(SiteItf father) throws RemoteException {
+		if(father == null) return;
+		if(this.fathers.contains(father)) return;
+		if(this.equals(father)) return;
+		
+		this.fathers.add(father);
 		father.addSon(this);
 	}
 
@@ -144,4 +221,22 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 	public void removeSon(SiteItf son) throws RemoteException {
 		this.children.remove(son);
 	}
+
+	@Override
+	public void addAllFathers(List<SiteItf> fathers) throws RemoteException {
+		for(SiteItf father : fathers){
+			this.addFather(father);
+		}
+	}
+
+	@Override
+	public void removeFather(SiteItf father) throws RemoteException {
+		this.fathers.remove(father);
+	}
+
+	@Override
+	public boolean equals(SiteItf other) throws RemoteException {
+		return this.hashCode() == other.hashCode();
+	}
+
 }
--- tp/RMI/src/SiteItf.java
+++ tp/RMI/src/SiteItf.java
@@ -1,10 +1,15 @@
+import java.util.List;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 
 public interface SiteItf extends Remote {
-	public void sendMessage(String Message) throws RemoteException;
+	
+	public boolean equals(SiteItf other) throws RemoteException;
+	public void sendMessage(List<SiteItf> from, String message) throws RemoteException;
 	public void addSon(SiteItf son) throws RemoteException;
 	public void removeSon(SiteItf son) throws RemoteException;
-	public void setFather(SiteItf father) throws RemoteException;
+	public void addFather(SiteItf father) throws RemoteException;
+	public void addAllFathers(List<SiteItf> fathers) throws RemoteException;
+	public void removeFather(SiteItf father) throws RemoteException;
 }
